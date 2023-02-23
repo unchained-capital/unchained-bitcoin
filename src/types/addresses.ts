6a2f2f0628ff3ab40e8f0a4364ba59496ca3da1a
Buck Perley <buck@unchained-capital.com>
2023-02-23 15:27:50 -0600

    fix: typos ☕️

--- src/types/addresses.ts
+++ src/types/addresses.ts
@@ -1,5 +1,5 @@
-// P2TR not able to be used anywhere yet but technicall a valid multisig
+// P2TR not able to be used anywhere yet but technically a valid multisig
 // address type.
-// We should be able to replace this with use of the MULTISIG_ADDREESS_TYPES
+// We should be able to replace this with use of the MULTISIG_ADDRESS_TYPES
 // enum when that file (./multisig.js) gets converted to typescript
 export type MultisigAddressType = "P2SH" | "P2WSH" | "P2SH-P2WSH" | "P2TR";
