029db9a2fa7e6b6b34cc8bff77205c06f48ab109
nk1tz <nate@unchained.com>
2023-09-15 10:39:20 -0700

    script

--- src/script.ts
+++ src/script.ts
@@ -1,16 +1,16 @@
 /**
  * This module provides functions for converting generic bitcoin
  * scripts to hex or opcodes.
- * 
+ *
  * @module script
  */
 
-import {script} from "bitcoinjs-lib";
+import { script } from "bitcoinjs-lib";
 
 /**
  * Extracts the ASM (opcode) representation of a script from a
  * `Multisig` object.
- * 
+ *
  * @param {Multisig} multisig - Multisig object
  * @returns {string} ASM representation of script
  * @example
@@ -20,7 +20,7 @@ import {script} from "bitcoinjs-lib";
  * } from "unchained-bitcoin";
  * const multisig = generateMultisigFromPublicKeys(MAINNET, P2SH, 1, "03a...", "03b...");
  * console.log(scriptToOps(multisig)) // "OP_1 03a... 03b... OP_2 OP_CHECKMULTISIG"
- * 
+ *
  */
 export function scriptToOps(multisig) {
   return script.toASM(multisig.output);
@@ -29,7 +29,7 @@ export function scriptToOps(multisig) {
 /**
  * Extracts the hex representation of a script from a `Multisig`
  * object.
- * 
+ *
  * @param {Multisig} multisigScript - Multisig object
  * @returns {string} hex representation of script
  * @example
@@ -39,8 +39,8 @@ export function scriptToOps(multisig) {
  * } from "unchained-bitcoin";
  * const multisig = generateMultisigFromPublicKeys(MAINNET, P2SH, 1, "03a...", "03b...");
  * console.log(scriptToHex(multisig)) // "5121024f355bdcb...5871aa52ae"
- * 
+ *
  */
 export function scriptToHex(multisigScript) {
-  return multisigScript.output.toString('hex');
+  return multisigScript.output.toString("hex");
 }
